a5424d006b196bae9f1640b9c490ec06cc19675f
Tanay Desai <tanaydesai26@gmail.com>
2020-12-18 10:38:03 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,134 @@
+var arr2,
+  arr1 = ["Chemical Engineer", "Process Designer", "Hobbyist Programmer"];
+var elem1 = document.getElementById("changeText");
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+const recFunc = () => {
+  return new Promise((resolve2) => {
+    for (let i = 0, p1 = Promise.resolve(); i < arr1.length; i++) {
+      p1 = p1.then(
+        () =>
+          new Promise((resolve1) => {
+            for (let j = 0, p = Promise.resolve(); j <= arr1[i].length * 2; j++) {
+              p = p.then(
+                () =>
+                  new Promise((resolve) => {
+                    setTimeout(function () {
+                      if (j <= arr1[i].length) {
+                        arr2 = arr1[i].slice(0, j);
+                        elem1.setAttribute("value", arr2);
+                        if (j == arr1[i].length) {
+                          document.getElementById("curser").setAttribute("class", "blink");
+                          setTimeout(() => {
+                            document.getElementById("curser").setAttribute("class", "");
+                            resolve();
+                          }, 300);
+                        } else {
+                          resolve();
+                        }
+                      } else {
+                        arr2 = arr1[i].slice(0, arr1[i].length * 2 - j);
+                        elem1.setAttribute("value", arr2);
+                        resolve();
+                        if (arr1[i].length * 2 - j == 0) {
+                          resolve1();
+                          if (i == arr1.length - 1) {
+                            resolve2();
+                          }
+                        }
+                      }
+                    }, 60);
+                  })
+              );
+            }
+          })
+      );
+    }
+  });
+};
+async function recRep() {
+  await recFunc();
+  recRep();
+}
+recRep();
+
+// another method for the typing effect!!
+
+// var i=1,j=1;
+// const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+// async function asyncForEach(array, callback) {
+//     for (let index = 0; index < array.length; index++) {
+//       await callback(array[index], index, array);
+//     }
+//   }
+// async function recursiveRepetition(){
+//     await asyncForEach(arr1,async (elem)=>{
+//         await waitFor(150);
+//         await displayText(elem)
+//     })
+//     recursiveRepetition();
+// }
+
+// recursiveRepetition();
+// function displayText(str1){
+//     return new Promise(async (res,rej)=>{
+//         var str2=[];
+//         await asyncForEach(str1.split(""),async (elem,i)=>{
+//             await waitFor(60);
+//             str2.push(elem)
+//             elem1.setAttribute('value',str2.join(""))
+//             // if(i==str1.split('').len")gth-1) res();
+//         })
+//         document.getElementById('curser').setAttribute('class','blink')
+//         await waitFor(600);
+//         document.getElementById('curser').setAttribute('class','')
+//         await asyncForEach(str1.split(""),async (elem,i)=>{
+//             await waitFor(60);
+//             str2.pop()
+//             elem1.setAttribute('value',str2.join(""))
+//             if(i==str1.split('').length-1) res();
+//         })
+//     })
+// }
+
+// ripples effect
+// $(document).ready(function() {
+// 	try {
+// 		$('body').ripples({
+// 			resolution: 600,
+// 			dropRadius: 10, //px
+// 			perturbance: 0.09,
+// 		});
+// 		$('main').ripples({
+// 			resolution: 514,
+// 			dropRadius: 10, //px
+// 			perturbance: 0.04,
+// 			interactive: true
+// 		});
+// 	}
+// 	catch (e) {
+// 		$('.error').show().text(e);
+//     }
+// })
+
+// particles effect
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+elem1 = document.getElementById("about");
+window.onscroll = myFunction;
+//     console.log('Hello')
+// }
+// elem1.addEventListener("scroll",myFunction)
+var counter = 0;
+function myFunction() {
+  if (counter == 0) {
+    if (window.pageYOffset > 700) {
+      console.log("hello");
+      counter++;
+    }
+  }
+}
